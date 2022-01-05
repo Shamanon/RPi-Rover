ec63b0d2d508c2891bedf78be4e9e5d3e78cc4c2
Joshua Besneatte <besneatte@gmail.com>
2022-01-04 22:23:47 -0800

    Test file for tonal buzzer

---
+++ tests/buzzer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from gpiozero import TonalBuzzer
+from gpiozero.tones import Tone
+from time import sleep
+
+b = TonalBuzzer(20)
+
+for i in range(60,70):
+	b.play(Tone(midi=float(i))) # frequency
+	sleep(.1)
+
+#for i in range(220,440):
+#	b.play(Tone(frequency=int(i))) # frequency
+#	sleep(.01)
